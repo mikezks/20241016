739815d6fd5d2a2b5326cccd16c6387e55c7c90d
mikezks <23002661+mikezks@users.noreply.github.com>
2024-10-21 11:22:44 +0200

    feat: passenger edit rxjs signal interop, ngrx select signal

--- projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
@@ -34,7 +34,7 @@ export class FlightSearchComponent {
     3: true,
     5: true
   };
-  protected flights$ = this.ticketsFacade.flights$;
+  protected flights = this.ticketsFacade.flights;
 
   constructor() {
     effect(() => {
--- projects/flight/src/app/checkin/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ projects/flight/src/app/checkin/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,7 +1,11 @@
 import { NgIf } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, effect, inject, input, numberAttribute, signal } from '@angular/core';
+import { takeUntilDestroyed, toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { validatePassengerStatus } from '../../util-validation';
+import { initialPassenger } from '../../logic-passenger';
+import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
+import { switchMap } from 'rxjs';
 
 
 @Component({
@@ -14,6 +18,17 @@ import { validatePassengerStatus } from '../../util-validation';
   templateUrl: './passenger-edit.component.html'
 })
 export class PassengerEditComponent {
+  destroyRef = inject(DestroyRef);
+  passengerService = inject(PassengerService);
+
+  id = input.required<number>();
+  passenger = toSignal(
+    toObservable(this.id).pipe(
+      switchMap(id => this.passengerService.findById(id)),
+      // takeUntilDestroyed(this.destroyRef)
+    ), { initialValue: initialPassenger }
+  );
+
   protected editForm = inject(NonNullableFormBuilder).group({
     id: [0],
     firstName: [''],
@@ -23,6 +38,15 @@ export class PassengerEditComponent {
       validatePassengerStatus(['A', 'B', 'C'])
     ]]
   });
+
+  constructor() {
+    effect(
+      () => this.editForm.patchValue(this.passenger())
+    );
+    this.destroyRef.onDestroy(
+      () => console.log('DESTROY Passenger Edit')
+    );
+  }
 
   protected save(): void {
     console.log(this.editForm.value);
